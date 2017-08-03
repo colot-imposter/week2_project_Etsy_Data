352d547e5fd650bfbb3e8f9724c15e536f5400b0
Colton Dowling <Colton@cd3style.com>
2017-08-03 14:42:41 -0500

    did it up with foreach loops borah

--- main.js
+++ main.js
@@ -7,61 +7,83 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-function question1 () {
-  let total= 0
-  for (let i = 0; i < data.length; i++) {
-    total +=data[i].price
+function question1() {
+    let total = 0
+    data.forEach(function(item) {
+        total += item.price;
+    let averagePrice = (total / data.length);
 
-
-  }
-
-let averagePrice = total/ data.length
-return averagePrice
-  // Answer:
+    console.log("The Average Price is " + averagePrice)
+    return averagePrice;
+  })
 }
-
-
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
-  // Answer:
-let specifPrice =[]
-for (var i = 0; i < data.length; i++) {
-   data[i] >14 && data[i]<18;
-   data[i]+= specifPrice[i];
+function question2() {
+    let specifPrice = []
+    data.forEach(function(item) {
+        if (item.price >= 14 && item.price <= 18)
+            specifPrice.push(item.title)
+    })
+    specifPrice.forEach(function(item) {
+        console.log(item);
+    })
 }
-}
-
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
-  Answer:
-  for (var i = 0; i < data.length; i++) {
-    if data.currency_code= 'GBP'{
-    data[i] =  {
-    return data
+function question3() {
+    data.forEach(function(currency) {
+        if (currency.currency_code === 'GBP') {
+            console.log(currency.title+ " costs "+ currency.price+ " GB Pounds");
+            // console.log(item.title + " costs " + item.price + " pounds");
+            }
+      })
     }
+
+
+    // 4: Display a list of all items who are made of wood.
+    function question4() {
+        // Answer:
+        data.forEach(function(wooders){
+          if (wooders.materials.includes('wood')) {
+          console.log(wooders.title +' is made of wood')
+        }
+      } );
+
   }
 
-}
+
+    // 5: Which items are made of eight or more materials?
+    // Display the name, number of items and the items it is made of.
+    function question5() {
+      data.forEach(function (eighters){
+        let numbEighters = eighters.materials.length;
+        if (numbEighters >= 8){
+
+            console.log(eighters.title +" is made up of "+ eighters.materials.length +" different materials, including: ");
+
+            eighters.materials.forEach(function(watAteTho)
+            {console.log("-" + watAteTho)
+
+        })
 
 
-// 4: Display a list of all items who are made of wood.
-function question4 () {
-  // Answer:
-}
+
+        }
+      })
+  }
 
 
-// 5: Which items are made of eight or more materials?
-   Display the name, number of items and the items it is made of.
-function question5 () {
-  // Answer:
-}
 
-
-// 6: How many items were made by their sellers?
-Answer:
-function question6 () {
-  // Answer:
+    // 6: How many items were made by their sellers?
+    function question6() {
+        // Answer:
+        let total = 0
+        data.forEach(function(selfmadesuperstats){
+          if (selfmadesuperstats.who_made === "i_did") {
+            total += 1
+          }
+      })
+console.log(total + " items were made by their sauce-ass owners. Themselves. Starters. Do-ers. Movers Broah. Each beer mug at a time. Broah.");
 }
